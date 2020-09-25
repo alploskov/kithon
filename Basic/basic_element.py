1d6b42f7aa6aeed7eb9836242f25147ed4f8722c
Alekseyploskov <59963354+aleksey-satir@users.noreply.github.com>
2020-09-25 23:02:06 +0300

    Update basic_element.py

--- Basic/basic_element.py
+++ Basic/basic_element.py
@@ -1,5 +1,20 @@
+#    This file is part of Python-Universal-Trnslator.
+#
+#    Python-Universal-Trnslator is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    Python-Universal-Trnslator is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with Python-Universal-Trnslator.  If not, see <https://www.gnu.org/licenses/>.
+
 import _ast
-#from Lib import *
+from Lib import *
 from Basic.execution_environment_api import function_analog
 
 signs={_ast.Add:"+",
