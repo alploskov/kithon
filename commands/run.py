492341840ee95e9ea819e7262b3cd279bd055e79
alploskov <59963354+aleksey-satir@users.noreply.github.com>
2022-07-18 23:45:29 +0300

    run command

---
+++ commands/run.py
@@ -0,0 +1,51 @@
+import os
+from typing import Optional
+import typer
+from jinja2 import Template
+from kithon import Transpiler
+from . import configurator
+try:
+    import packed
+except:
+    packed = None
+
+
+def _run(
+    file_name: str = typer.Argument(
+        None,
+        metavar='FILE',
+        help='Name of file for transpilation'
+    ),
+    templates: list[str] = configurator.templates,
+    macro: list[str] = configurator.macro,
+    target: Optional[str] = configurator.to,
+    input_lang: Optional[str] = typer.Option(
+        '',
+        '-l',
+        '--lang',
+        help='Marking the entrance language (py, hy, coco)',
+        show_default='Determined by the filename'
+    ),
+):
+    """
+    Transpile python code into chose language
+    """
+    transpiler = Transpiler()
+    configurator.conf(
+        transpiler, target, macro, templates
+    )
+    if os.path.isdir(file_name):
+        return
+    else:
+        ext = file_name.split('.')[-1]
+        code = open(file_name, 'r').read()
+        if ext.endswith('x') and packed is not None:
+            code = packed.translate(code)
+        lang = input_lang or ext.removesuffix('x')
+        os.system(
+            Template(transpiler.templates['meta']['run']).render(
+                code=transpiler.generate(
+                    code, lang=lang
+                )
+            )
+        )
