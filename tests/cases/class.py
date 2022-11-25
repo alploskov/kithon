682a1f1f6903ab02c2292a313d8049464f803e6a
alploskov <ploskov-ai@yandex.ru>
2022-11-25 15:42:31 +0300

    tests: update class case

--- tests/cases/class.py
+++ tests/cases/class.py
@@ -9,11 +9,18 @@ class A:
     def e(self):
         return self.b
 
+    def f(self):
+        self.b = 20
+        self.c = 200
+
 def main():
     a = A()
     print(a.b)
     print(a.c)
     print(a.d())
     print(a.e())
+    a.f()
+    print(a.b)
+    print(a.c)
 
 main()
