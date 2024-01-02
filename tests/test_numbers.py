97f0d77cdeb1053dc8499a756c98eda40b1dcac9
alploskov <ploskov-ai@yandex.ru>
2024-01-03 00:25:58 +0300

    tests: test bit operations(&, |, ^, <<, >>, ~)

--- tests/test_numbers.py
+++ tests/test_numbers.py
@@ -22,3 +22,22 @@ def test_op():
     ]
     check_exprs(exprs)
 
+def test_bit_ops():
+    nums = [-10, -2, -1, 0, 1, 2, 10]
+    exprs = []
+    for n1 in nums:
+        for n2 in nums:
+            exprs.append(f'{n1} & {n2}')
+            exprs.append(f'{n2} & {n1}')
+            exprs.append(f'{n1} | {n2}')
+            exprs.append(f'{n2} | {n1}')
+            exprs.append(f'{n1} ^ {n2}')
+            exprs.append(f'{n2} ^ {n1}')
+            if n2 > 0:
+                exprs.append(f'{n1} << {n2}')
+                exprs.append(f'{n1} >> {n2}')
+            if n1 > 0:
+                exprs.append(f'{n2} << {n1}')
+                exprs.append(f'{n2} >> {n1}')
+        exprs.append(f'~{n1}')
+    check_exprs(exprs)
